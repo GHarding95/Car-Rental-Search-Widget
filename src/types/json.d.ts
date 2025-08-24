5e2c60b87921d6738dfde18223cdcc84b1ff2ab7
Glen <59471231+GHarding95@users.noreply.github.com>
2025-08-24 17:27:24 +0100

    fixed type error

--- src/types/json.d.ts
+++ src/types/json.d.ts
@@ -2,3 +2,10 @@ declare module "*.json" {
   const value: unknown
   export default value
 }
+
+// Specific declaration for locations.json
+declare module "../../assets/Locations.json" {
+  import { LocationsData } from "../types"
+  const value: LocationsData
+  export default value
+}
